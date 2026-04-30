ab20a4a245e551221abcddedc496433e46fd9ba6
Luis-esa <luisedusa265@gmail.com>
2026-04-30 00:21:37 -0300

    first commit

---
+++ src/main/java/br/edu/ifs/sistemaacademicointegradoapi/SistemaAcademicoIntegradoApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.sistemaacademicointegradoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcademicoIntegradoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcademicoIntegradoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifs/sistemaacademicointegradoapi/SistemaAcademicoIntegradoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.sistemaacademicointegradoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademicoIntegradoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
