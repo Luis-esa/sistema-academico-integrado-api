381eb269795f7b9f62065bb4245e658215bdd528
Luis-esa <luisedusa265@gmail.com>
2026-05-17 11:42:47 -0300

    refactor(repository): atualiza interfaces para utilizar métodos de busca customizados com .find

--- src/main/java/br/edu/ifs/sistemaacademicointegradoapi/repository/TurmaRepository.java
+++ src/main/java/br/edu/ifs/sistemaacademicointegradoapi/repository/TurmaRepository.java
@@ -4,7 +4,11 @@ import br.edu.ifs.sistemaacademicointegradoapi.model.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
+
+    List<Turma> findByProfessorId(Long professorId);
 
 }
